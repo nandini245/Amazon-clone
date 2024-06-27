290421d60e4c0bbacbb2f3cc5c2053d8f03912a6
nandini245 <142869274+nandini245@users.noreply.github.com>
2024-06-27 21:58:30 +0530

    Delete backend/Users.js

--- backend/Users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = mongoose.Schema({
-  fullName: String,
-  email: String,
-  password: String,
-});
-
-module.exports = mongoose.model("users", UserSchema);
